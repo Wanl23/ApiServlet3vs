dc53acd7b280fb13bf78dc2e8223167694c79500
Wanl23 <ivan.leschinsky@yandex.ru>
2018-05-15 23:32:20 +0500

    added repository

--- ApiServlet3vs/src/Servlet/xmlToJson.java
+++ ApiServlet3vs/src/Servlet/xmlToJson.java
@@ -1,43 +1,47 @@
 package Servlet;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
+import javax.jws.WebService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class xmlToJson
- */
-@WebServlet("/xmlToJson")
-public class xmlToJson extends HttpServlet {	
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public xmlToJson() {
-        super();
-        // TODO Auto-generated constructor stub
+import com.sun.corba.se.spi.activation.Repository;
+
+import Repository.xmlRepository;
+
+@WebServlet ("/api/spxtags")
+public class xmlToJson extends HttpServlet {
+
+    xmlRepository xmlRep = new xmlRepository();    
+    Map<String, String> json = null;
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+    	//here will code witch should send json to client
+    	    	
+//    	try {
+//			json = xmlRep.getMap();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//    	//PrintWriter out = response.getWriter();
+//    	for(Map.Entry<String, String> entry : json.entrySet()) 
+//    	{
+//    		System.out.println(entry.getKey() + "=" + entry.getValue());
+//    	}
+//    	//out.close();
+    	
+ 
+        // 6. Send List<Article> as JSON to client
+        //json.writeValue(response.getOutputStream());
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
