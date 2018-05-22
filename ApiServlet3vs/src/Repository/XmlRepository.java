158c95cb38b4d65ecb0645f65493b4dec143baa7
Wanl23 <ivan.leschinsky@yandex.ru>
2018-05-22 13:16:25 +0500

    Finish update

---
+++ ApiServlet3vs/src/Repository/XmlRepository.java
@@ -0,0 +1,93 @@
+package Repository;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+public class XmlRepository { 	
+	
+	static final String SPF_SCHEMA = "/SCHEMA";
+	private ArrayList<String> paths = new ArrayList<>();
+	
+	//putting all maps after parsing in one
+	public String getSpxTags() {
+		getFilePaths(new File(SPF_SCHEMA));
+		Map<String, String> dataMap = new HashMap<String, String>();
+		for(String path: paths) {
+			try {
+				dataMap.putAll(getDataFromFile(path));
+			} catch (Exception e) {
+				//log error
+				e.printStackTrace();
+			}
+		}
+		
+		return mapToJson(dataMap);
+	}
+	//convert map to json format	
+	private String mapToJson(Map<String, String> map) {		
+		String json = "{";
+		
+		if(!map.isEmpty()) {
+			for(Map.Entry<String, String> entry : map.entrySet()) {
+			    String uid = entry.getKey();
+			    String name = entry.getValue();
+			    json += String.format("[uid:%s, name:%s], " + "\n", uid, name);
+			}
+			json = json.substring(0, json.length() - 3);//del last 3 simvols
+		}
+		
+		return json += "}";
+	} 
+		//parsing xml file
+		private Map<String, String> getDataFromFile(String filePath) throws ParserConfigurationException, IOException, SAXException {
+        Map<String, String> map = new HashMap<String, String>();
+
+        File xmlFile = new File(filePath);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = factory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+        NodeList nodeList = doc.getElementsByTagName("EnumEnum");
+        for(int i = 0; i < nodeList.getLength(); i++) {
+            if(nodeList.item(i).getNodeType() == Element.ELEMENT_NODE) {
+                Element node = (Element)nodeList.item(i);
+                NodeList childList = node.getChildNodes();
+                for(int j = 0; j < childList.getLength(); j ++){
+                    if(childList.item(j).getNodeType() == Element.ELEMENT_NODE) {
+                        Element childListElement = (Element) childList.item(j);
+                        String uid = childListElement.getAttribute("UID");
+                        String name = childListElement.getAttribute("Name");
+                        map.put(uid, name);
+                    }
+                }
+            }
+        }
+        return map;
+    }
+	//get absolute paths to all files
+	private void getFilePaths(File folder)
+	{
+	    File[] folderEntries = folder.listFiles();
+	    for (File entry : folderEntries)
+	    {
+	        if (entry.isDirectory())
+	        {
+	        	getFilePaths(entry);
+	            continue;
+	        }
+	        else {
+	        	paths.add(entry.getAbsolutePath());
+	        }
+	        
+	    }
+	}
+}
--- ApiServlet3vs/src/Servlet/XmlToJson.java
+++ ApiServlet3vs/src/Servlet/XmlToJson.java
@@ -1,47 +1,23 @@
 package Servlet;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import javax.jws.WebService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.sun.corba.se.spi.activation.Repository;
-
-import Repository.xmlRepository;
+import Repository.XmlRepository;
 
 @WebServlet ("/api/spxtags")
-public class xmlToJson extends HttpServlet {
-
-    xmlRepository xmlRep = new xmlRepository();    
-    Map<String, String> json = null;
+public class XmlToJson extends HttpServlet { 
+	
+	private XmlRepository xmlRepository = new XmlRepository();
     
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
-    	//here will code witch should send json to client
-    	    	
-//    	try {
-//			json = xmlRep.getMap();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//    	//PrintWriter out = response.getWriter();
-//    	for(Map.Entry<String, String> entry : json.entrySet()) 
-//    	{
-//    		System.out.println(entry.getKey() + "=" + entry.getValue());
-//    	}
-//    	//out.close();
-    	
- 
-        // 6. Send List<Article> as JSON to client
-        //json.writeValue(response.getOutputStream());
-    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {    	
+    	 
+    	String json = xmlRepository.getSpxTags();
+		
+		response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+	    response.getWriter().write(json);
+	}
 }
